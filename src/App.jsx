9554ae8d85ac4c08cc4321cde192b5e1e7c54d0d
Niki Jeftic <nikijeftic123@gmail.com>
2022-11-19 10:56:48 +0100

    Ovo druga promena direktno na main

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import Carousel from './components/Carousel/Carousel';
 
 const App = () => (
   <Routes>
-    Cao luka ovo je nasa prva promena
+    Cao Luka ovo je nasa prva promena Cao Luka ovo je nasa druga promena ali
+    direktno na main
     <Route path="/" element={<Carousel />} />
   </Routes>
 );
