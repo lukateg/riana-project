cdd5bb3bae944d28a0b008bf01710855ac76d9aa
Luka <ltegeltija@griddynamics.com>
2022-11-23 16:59:20 +0100

    Yacht-experience desktop version

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 // import { Routes, Route } from 'react-router-dom';
 // import Carousel from './components/Carousel/Carousel';
 import Navbar from './components/Navbar/Navbar';
+import YachtExperience from './components/YachtExperience/YachtExperience';
 
 const App = () => (
   // <Routes>
   //   <Route path="/" element={<Carousel />} />
   // </Routes>
-  <Navbar />
+  <>
+    <Navbar />
+    <YachtExperience />
+  </>
 );
 
 export default App;
