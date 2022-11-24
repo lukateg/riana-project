a5f7e3d10098cf24304eec69831616eca40922ad
Niki Jeftic <nikijeftic123@gmail.com>
2022-11-24 12:56:28 +0100

    Navbar je gotov, jedino sto nije isto kao sa dizajna jeste logo. CSS za logo dobijam naknadno i onda to ispravljam

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,27 @@
 import styles from './Navbar.module.scss';
 
-const Navbar = () => <h1 className={styles.naslov}>RIANA YACHT</h1>;
+const Navbar = () => (
+  <nav>
+    <h1 className={styles.nav__headline}>riana yacht</h1>
+    <hr className={styles.horizontalLine} />
+    <div className={styles.nav__links}>
+      <div className={styles.nav__item}>
+        <a href="home">home</a>
+      </div>
+      <div className={styles.nav__item}>
+        <a href="gallery">gallery</a>
+      </div>
+      <div className={styles.nav__item}>
+        <a href="stories">stories</a>
+      </div>
+      <div className={styles.nav__item}>
+        <a href="offers">offers</a>
+      </div>
+      <div className={styles.nav__item}>
+        <a href="contact">contact</a>
+      </div>
+    </div>
+  </nav>
+);
+
 export default Navbar;
