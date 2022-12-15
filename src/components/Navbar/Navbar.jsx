df4dd54a1fcf1d4cd5fce916eb6fe8ebf3dae360
Niki Jeftic <nikijeftic123@gmail.com>
2022-12-15 15:10:28 +0100

    Navbar je gotov

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import styles from './Navbar.module.scss';
 
 const Navbar = () => {
   const [navbar, setNavbar] = useState(false);
+  const [open, setOpen] = useState(true);
 
   const changeBackground = () => {
     if (window.scrollY >= 80) {
@@ -11,24 +12,40 @@ const Navbar = () => {
       setNavbar(false);
     }
   };
+
+  const changeIcon = () => {
+    setOpen(!open);
+  };
   window.addEventListener('scroll', changeBackground);
   return (
     <nav
       className={
-        navbar ? `${styles.navbar} ${styles.active}` : `${styles.navbar}`
+        navbar || !open
+          ? `${styles.navbar} ${styles.active}`
+          : `${styles.navbar}`
       }
     >
-      <h1
+      <div
         className={
-          navbar
+          navbar || !open
             ? `${styles.nav__headline} ${styles.active}`
             : `${styles.nav__headline}`
         }
       >
-        riana yacht
-      </h1>
+        <h1>riana yacht</h1>
+        <i
+          className={open ? 'fas fa-bars icon-3x' : 'fas fa-times'}
+          onClick={changeIcon}
+          role="presentation"
+          id={navbar ? 'nav-icon-acitve' : 'nav-icon'}
+        >
+          {/* icon */}
+        </i>
+      </div>
       <hr
-        className={navbar ? `${styles.hr} ${styles.active}` : `${styles.hr}`}
+        className={
+          navbar && open ? `${styles.hr} ${styles.active}` : `${styles.hr}`
+        }
       />
       <div
         className={
@@ -96,6 +113,42 @@ const Navbar = () => {
           >
             contact
           </a>
+        </div>
+      </div>
+      <div
+        className={
+          !open
+            ? `${styles.nav__mobile} ${styles.active}`
+            : `${styles.nav__mobile}`
+        }
+      >
+        <div className={styles.nav__mobile__row__contact}>
+          <i className="fa-solid fa-phone">{/* phone */}</i>
+          <i className="fa-regular fa-envelope">{/* mail */}</i>
+        </div>
+        <div className={styles.nav__mobile__row}>
+          <a href="newsletter signup">newsletter signup</a>
+          <i className="fa-solid fa-chevron-right">{/* arrow */}</i>
+        </div>
+        <div className={styles.nav__mobile__row}>
+          <a href="gallery">gallery</a>
+          <i className="fa-solid fa-chevron-right">{/* arrow */}</i>
+        </div>
+        <div className={styles.nav__mobile__row}>
+          <a href="stories">stories</a>
+          <i className="fa-solid fa-chevron-right">{/* arrow */}</i>
+        </div>
+        <div className={styles.nav__mobile__row}>
+          <a href="offers">offers</a>
+          <i className="fa-solid fa-chevron-right">{/* arrow */}</i>
+        </div>
+        <div className={styles.nav__mobile__row}>
+          <a href="events">events</a>
+          <i className="fa-solid fa-chevron-right">{/* arrow */}</i>
+        </div>
+        <div className={styles.nav__mobile__row}>
+          <a href="contact">contact</a>
+          <i className="fa-solid fa-chevron-right">{/* arrow */}</i>
         </div>
       </div>
     </nav>
