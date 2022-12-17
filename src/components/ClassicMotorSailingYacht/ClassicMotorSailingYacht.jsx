efa6094c731b926ff5bdd805ef80cadd1861e5d7
Niki Jeftic <nikijeftic123@gmail.com>
2022-12-17 17:43:47 +0100

    dodao funkcionalnost na dugme

--- src/components/ClassicMotorSailingYacht/ClassicMotorSailingYacht.jsx
+++ src/components/ClassicMotorSailingYacht/ClassicMotorSailingYacht.jsx
@@ -3,7 +3,13 @@ import styles from './ClassicMotorSailingYacht.module.scss';
 const ClassicMotorSailingYacht = () => (
   <div className={styles.section}>
     <h4 className={styles.section__heading}>CLASSIC MOTOR SAILING YACHT</h4>
-    <div className={styles.button}>EXPLORE 360 VIRTUAL TOUR</div>
+    <a
+      href="http://virtualtour.hr/riana/"
+      target="_blank"
+      rel="noreferrer noopener"
+    >
+      <div className={styles.button}>EXPLORE 360 VIRTUAL TOUR</div>
+    </a>
   </div>
 );
 
