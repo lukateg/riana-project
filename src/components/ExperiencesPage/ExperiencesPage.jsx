aa260bb0a4b3caca6e8c0064ccb6d840c5487c48
Niki Jeftic <nikijeftic123@gmail.com>
2023-03-11 20:05:07 +0100

    changes

--- src/components/ExperiencesPage/ExperiencesPage.jsx
+++ src/components/ExperiencesPage/ExperiencesPage.jsx
@@ -15,84 +15,119 @@ import it2 from '../../images/it2.jpg';
 import it3 from '../../images/it3.jpg';
 import Reservations from '../Reservations/Reservations';
 
-const ExperiencesPage = () => (
-  <>
-    <HeroImage image={image} />
-    <Breadcrumbs />
-    <div className={styles.heading}>
-      CRUISING THE ADRIATIC AND MEDITERRANEAN
-    </div>
+const ExperiencesPage = () => {
+  const conutries = [
+    {
+      backgorund: 'yes',
+      image: montegroImage,
+      heading: 'MONTENEGRO',
+      p1: 'Discover this tiny but unique country in the heart of Europe that is rich in nature and long in history, culture and heritage.',
+      p2: 'The Montenegrin coastline (from the Croatian to the Albanian border) abounds with stunning scenery, gastronomic delights, numerous anchorage and swimming spots and water access to the UNESCO World Heritage protected towns of Kotor, Budva, Bar and Ulcijn amongst other must visit locations including numerous mountains, lakes, forests and other towns and villages across the country.',
+      p3: 'Bespoke Montenegro land adventures and other services and experiences are offered in conjunction with our destination management partner Discover Montenegro, discovermontenegro.com.'
+    },
+    {
+      image: croatiaImage,
+      heading: 'CROATIA',
+      p1: 'Split, Dubrovnik & Kornati',
+      p2: 'At the doorstep to Montenegro lays Dalmatia and the beginning of the immense Croatian coastline. With over 1000 islands and coastlines lined with ancient historical towns & sites, Croatia is arguably one of the best sailing destinations in the world.',
+      p3: 'Croatia’s islands are remarkably close together and offer protected sailing that limits the need for long open water crossings between locations.'
+    },
+    {
+      backgorund: 'yes',
+      image: greeceImage,
+      heading: 'GREECE',
+      p1: 'Athens, Kos, Paros & Mykonos',
+      p2: 'Greece abounds with ancient history, clear, clean water, breathtaking islands, uncountable swimming and anchorage locations that all make for a perfect sailing vacation filled with unforgettable memories.',
+      p3: 'For pure relaxation or partying at the world-famous hotspots such as Nammos, Matsuhisa and Scorpios guests pick the destination and preferences and Riana Yacht will take you there.'
+    },
+    {
+      image: italyImage,
+      heading: 'ITALY',
+      p1: 'Italy needs little introduction as a sailing destination, however, we have a few tricks up our sleeve when it comes to navigating the various Italian hotspots and adding a selection of stunning locations and expeditions to your itinerary. ',
+      p2: 'For our volcano cruise, for example, we can offer a pure Sicilian dinner experience with a visit from a renowned local chef, local wines and live acoustic background music as you dine at sunset drifting past the active Stromboli volcano as your backdrop.',
+      p3: 'Our bespoke volcano dining experience is offered in conjunction with our Sicilian destination partner Contessa Consulting, contessaconsulting.com at an extra cost.'
+    }
+  ];
 
-    <CountryComponent
-      backgorund="yes"
-      image={montegroImage}
-      heading="MONTENEGRO"
-      p1="Discover this tiny but unique country in the heart of Europe that is rich in nature and long in history, culture and heritage."
-      p2="The Montenegrin coastline (from the Croatian to the Albanian border) abounds with stunning scenery, gastronomic delights, numerous anchorage and swimming spots and water access to the UNESCO World Heritage protected towns of Kotor, Budva, Bar and Ulcijn amongst other must visit locations including numerous mountains, lakes, forests and other towns and villages across the country."
-      p3="Bespoke Montenegro land adventures and other services and experiences are offered in conjunction with our destination management partner Discover Montenegro, discovermontenegro.com."
-    />
+  const itineraries = [
+    {
+      number: '1',
+      image: it2,
+      heading: 'DAY ONE',
+      t1: '• Sailing in the Boka Bay ',
+      t2: '• Swim near Our Lady of the Rock',
+      t3: '• Visit to the Blue Cave',
+      t4: '• Sunset sail',
+      t5: '• Dinner in Perast',
+      t6: '• Overnight in Kotor'
+    },
+    {
+      number: '2',
+      image: it3,
+      heading: 'DAY TWO',
+      t1: '• Sail to Budva ',
+      t2: '• Tour of the Budva old town',
+      t3: '• Sail around Sveti Stefan',
+      t4: '• Dinner in Budva',
+      t5: '• Overnight in Budva'
+    },
+    {
+      number: '3',
+      image: it1,
+      heading: 'DAY THREE',
+      t1: '• Sail to Ulcinj',
+      t2: '• Day at Ada Bojana, the longest sandy beach in Montenegro',
+      t3: '• Barbecue at the beach',
+      t4: '• Overnight in Ulcinj'
+    }
+  ];
 
-    <CountryComponent
-      image={croatiaImage}
-      heading="CROATIA"
-      p1="Split, Dubrovnik & Kornati"
-      p2="At the doorstep to Montenegro lays Dalmatia and the beginning of the immense Croatian coastline. With over 1000 islands and coastlines lined with ancient historical towns & sites, Croatia is arguably one of the best sailing destinations in the world."
-      p3="Croatia’s islands are remarkably close together and offer protected sailing that limits the need for long open water crossings between locations."
-    />
-    <CountryComponent
-      backgorund="yes"
-      image={greeceImage}
-      heading="GREECE"
-      p1="Athens, Kos, Paros & Mykonos"
-      p2="Greece abounds with ancient history, clear, clean water, breathtaking islands, uncountable swimming and anchorage locations that all make for a perfect sailing vacation filled with unforgettable memories."
-      p3="For pure relaxation or partying at the world-famous hotspots such as Nammos, Matsuhisa and Scorpios guests pick the destination and preferences and Riana Yacht will take you there."
-    />
-    <CountryComponent
-      image={italyImage}
-      heading="ITALY"
-      p1="Italy needs little introduction as a sailing destination, however, we have a few tricks up our sleeve when it comes to navigating the various Italian hotspots and adding a selection of stunning locations and expeditions to your itinerary. "
-      p2="For our volcano cruise, for example, we can offer a pure Sicilian dinner experience with a visit from a renowned local chef, local wines and live acoustic background music as you dine at sunset drifting past the active Stromboli volcano as your backdrop."
-      p3="Our bespoke volcano dining experience is offered in conjunction with our Sicilian destination partner Contessa Consulting, contessaconsulting.com at an extra cost."
-    />
-    <Reservations />
-    <img src={image2} alt="" className={styles.image} />
-    <div className={styles.itinerary}>
-      <h1 className={styles.itinerary__heading}>SUGGESTED ITINERARY</h1>
-      <div className={styles.itinerary__section}>
-        <Itinerary
-          number="1"
-          image={it2}
-          heading="DAY ONE"
-          t1="• Sailing in the Boka Bay "
-          t2="• Swim near Our Lady of the Rock"
-          t3="• Visit to the Blue Cave"
-          t4="• Sunset sail"
-          t5="• Dinner in Perast"
-          t6="• Overnight in Kotor"
-        />
-        <Itinerary
-          number="2"
-          image={it3}
-          heading="DAY TWO"
-          t1="• Sail to Budva "
-          t2="• Tour of the Budva old town"
-          t3="• Sail around Sveti Stefan"
-          t4="• Dinner in Budva"
-          t5="• Overnight in Budva"
-        />
-        <Itinerary
-          number="3"
-          image={it1}
-          heading="DAY THREE"
-          t1="• Sail to Ulcinj"
-          t2="• Day at Ada Bojana, the longest sandy beach in Montenegro"
-          t3="• Barbecue at the beach"
-          t4="• Overnight in Ulcinj"
-        />
+  return (
+    <>
+      <HeroImage image={image} />
+      <Breadcrumbs />
+      <div className={styles.heading}>
+        CRUISING THE ADRIATIC AND MEDITERRANEAN
       </div>
-    </div>
-    <DownloadBrochure />
-  </>
-);
+
+      {conutries.map((country, index) => (
+        <CountryComponent
+          // eslint-disable-next-line react/no-array-index-key
+          key={index}
+          backgorund={country.backgorund}
+          image={country.image}
+          heading={country.heading}
+          p1={country.p1}
+          p2={country.p2}
+          p3={country.p3}
+        />
+      ))}
+
+      <Reservations />
+      <img src={image2} alt="" className={styles.image} />
+      <div className={styles.itinerary}>
+        <h1 className={styles.itinerary__heading}>SUGGESTED ITINERARY</h1>
+        <div className={styles.itinerary__section}>
+          {itineraries.map((itinirary, index) => (
+            <Itinerary
+              // eslint-disable-next-line react/no-array-index-key
+              key={index}
+              number={itinirary.number}
+              image={itinirary.image}
+              heading={itinirary.heading}
+              t1={itinirary.t1}
+              t2={itinirary.t2}
+              t3={itinirary.t3}
+              t4={itinirary.t4}
+              t5={itinirary.t5}
+              t6={itinirary.t6}
+            />
+          ))}
+        </div>
+      </div>
+      <DownloadBrochure />
+    </>
+  );
+};
 
 export default ExperiencesPage;
